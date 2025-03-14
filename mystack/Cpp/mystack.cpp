d4fb3c79595b23167e14c96382e4a730cf0c3201
Julian Bustamante N <julianbustamantenarvaez@gmail.com>
2025-03-14 16:33:53 -0500

    Added C++ stack implementation with vector-based storage

--- mystack/Cpp/mystack.cpp
+++ mystack/Cpp/mystack.cpp
@@ -1,1 +1,56 @@
-.
+#include "mystack.hpp"
+#include <iostream>
+
+int  Stack::size() {
+    return stack_array.size();
+}
+
+bool Stack::isEmpty() {
+    return size() <= 0;
+}
+
+int  Stack::top() {
+    return this->stack_array.back();
+}
+
+bool Stack::resize() {
+
+    capacity *= 2;  // Double the capacity
+    stack_array.reserve(capacity); // reserve space without initializing elem
+    std::cout << "Stack resized to " << capacity << "\n";
+
+    return true;
+}
+
+bool Stack::push(int value) {
+
+    if(size() == this->capacity) {
+        resize();
+    }
+
+    this->stack_array.push_back(value);
+    return true;
+}
+
+bool Stack::pop() {
+    if (!isEmpty()) {
+        this->stack_array.pop_back();
+        return true;
+    } else {
+        std::cout << "Error: Stack is Empty\n";
+        return false;
+    }
+
+}
+
+void Stack::printStack() {
+    for (int i:this->stack_array) {
+        std::cout << " " << i;
+    }
+
+    if (isEmpty()) {
+        printf("Stack is empty\n");
+    } else {
+        std::cout << std::endl;
+    }
+}
--- mystack/Cpp/mystack.hpp
+++ mystack/Cpp/mystack.hpp
@@ -1,1 +1,25 @@
-.
+#include <vector>
+
+
+class Stack {
+
+private:
+    std::vector<int> stack_array;
+    int capacity;
+
+public:
+
+    Stack(int capacity) {
+        this->capacity = capacity;
+        this->stack_array.reserve(this->capacity);
+    }
+
+    int  size();
+    bool isEmpty();
+    int  top();
+    bool resize();
+    bool push(int value);
+    bool pop();
+    void printStack();
+
+};
